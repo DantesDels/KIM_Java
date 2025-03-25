aa760e7179ecde68b70e5fc60cd9632660086fb8
DantesDels <delver.sebastien@gmail.com>
2025-03-25 21:39:45 +0100

    feat(Discussion) : Discussion init, System.out.print need revisions.

--- src/main/java/fr/ynov/kim/domain/Discussion.java
+++ src/main/java/fr/ynov/kim/domain/Discussion.java
@@ -1,4 +1,42 @@
 package fr.ynov.kim.domain;
 
+import java.util.Scanner;
+
 public class Discussion {
+
+    public String title;
+    public Message startMessage;
+
+    public Discussion(String title, Message startMessage) {
+        this.title = title;
+        this.startMessage = startMessage;
+    }
+
+    public void displayReplies(Message currentMessage) {
+        for (Message reply : currentMessage.getReplies()) {
+            System.out.println(" - " + reply.getMsg());
+        }
+    }
+
+    public void start() {
+        System.out.println("Starting discussion " + this.title);
+        System.out.println(startMessage);
+        this.displayReplies(startMessage);
+        final Scanner scanner = new Scanner(System.in);
+
+        Message currentMessage = startMessage;
+        while(!currentMessage.getReplies().isEmpty()) {
+            int choice = scanner.nextInt();
+            try {
+                if (currentMessage.getReplies().get(choice - 1) != null) {
+                    currentMessage = currentMessage.getReplies().get(choice - 1);
+                    System.out.println("Chosen message is " + currentMessage.getMsg());
+                    this.displayReplies(currentMessage);
+                }
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Error - Invalid choice");
+            }
+        }
+        scanner.close();
+    }
 }
