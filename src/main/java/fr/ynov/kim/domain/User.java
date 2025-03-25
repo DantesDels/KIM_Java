c524c3931320ddabe6bd1113908cd601759d38b4
DantesDels <delver.sebastien@gmail.com>
2025-03-25 21:32:45 +0100

    feat(FakeUser) : User init, need constructor and else.

--- src/main/java/fr/ynov/kim/domain/User.java
+++ src/main/java/fr/ynov/kim/domain/User.java
@@ -1,4 +1,12 @@
 package fr.ynov.kim.domain;
 
 public class User extends Person {
+
+    private String preMadeMessages;
+
+    public User(String username, String profilPicture, String msgBio) {
+        super(username, profilPicture, msgBio);
+
+
+    }
 }
