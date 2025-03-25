6d894d4ca8259278bec8ae8a1288cb4f2fb9a18e
DantesDels <delver.sebastien@gmail.com>
2025-03-25 21:41:47 +0100

    feat(Message) : Message init, method getTime & getReply not called.

--- src/main/java/fr/ynov/kim/domain/Message.java
+++ src/main/java/fr/ynov/kim/domain/Message.java
@@ -1,4 +1,40 @@
 package fr.ynov.kim.domain;
 
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.time.LocalDateTime;
+
+
 public class Message {
+
+    private LocalDateTime time;
+    private String msg;
+    private List<Message> messages;
+
+    public Message(String msg, List<Message> messages) {
+        this.time = java.time.LocalDateTime.now();
+        this.msg = msg;
+        this.messages = messages;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public List<Message> getReplies() {
+        return messages;
+    }
+
+    public Message getReply(int index) {
+        return messages.get(index);
+    }
+
+    @Override
+    public String toString() {
+        return "Message [time = " + time.format(DateTimeFormatter.ofPattern("HH:mm")) + ", msg = " + msg + "]";
+    }
 }
