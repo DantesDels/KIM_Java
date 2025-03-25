a563b8f6bb6af486931e6bb4fd3d0198ea9a12bf
DantesDels <delver.sebastien@gmail.com>
2025-03-25 21:36:52 +0100

    feat(Application) : void main init, testing scenario = working.

--- src/main/java/fr/ynov/kim/main/Application.java
+++ src/main/java/fr/ynov/kim/main/Application.java
@@ -1,4 +1,16 @@
 package fr.ynov.kim.main;
 
+import fr.ynov.kim.domain.FakeUser;
+import fr.ynov.kim.domain.MessageUtils;
+import fr.ynov.kim.domain.Discussion;
+
 public class Application {
+
+    public static void main(String[] args) {
+        FakeUser f = MessageUtils.initiliazeUser();
+        Discussion d = f.getScript().get((int)(Math.random() * f.getScript().size()));
+        System.out.println("Chat with : " + f.getUsername());
+        d.start();
+
+    }
 }
