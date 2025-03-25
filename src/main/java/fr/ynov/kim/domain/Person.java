b47213129dd900f018d043d1138637c0acedc69e
DantesDels <delver.sebastien@gmail.com>
2025-03-25 21:34:15 +0100

    feat(Person) : Person abstract init, methods created but not called.

--- src/main/java/fr/ynov/kim/domain/Person.java
+++ src/main/java/fr/ynov/kim/domain/Person.java
@@ -1,4 +1,38 @@
 package fr.ynov.kim.domain;
 
 public abstract class Person {
+
+    private String username;
+    private String profilPicture;
+    private String msgBio;
+
+    public Person (String username, String profilPicture, String msgBio) {
+        this.username = username;
+        this.profilPicture = profilPicture;
+        this.msgBio = msgBio;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getProfilPicture() {
+        return profilPicture;
+    }
+
+    public String getMsgBio() {
+        return msgBio;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setProfilPicture(String profilPicture) {
+        this.profilPicture = profilPicture;
+    }
+
+    public void setMsgBio(String msgBio) {
+        this.msgBio = msgBio;
+    }
 }
