52d4e91bed7976ab8f057db90baa3b363dd18e22
DantesDels <delver.sebastien@gmail.com>
2025-03-25 21:30:12 +0100

    feat(FakeUser) : FakeUser init, need timerUntilNextConnection.

--- src/main/java/fr/ynov/kim/domain/FakeUser.java
+++ src/main/java/fr/ynov/kim/domain/FakeUser.java
@@ -1,4 +1,56 @@
+
 package fr.ynov.kim.domain;
 
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
 public class FakeUser extends Person {
+
+    private boolean connectionStatus;
+    private List<Discussion> script;
+    private Timer timerUntilNextConnection;
+
+    public FakeUser(String username, String profilPicture, String msgBio, List<Discussion> script) {
+        super(username, profilPicture, msgBio);
+
+        this.script = script;
+        this.connectionStatus = true;
+    }
+
+    public void setOnline () {
+        connectionStatus = true;
+        System.out.println(getUsername() + " is online");
+    }
+
+    public List<Discussion> getScript() {
+        return script;
+    }
+
+    private void reply () {
+        while (!script.isEmpty()) {
+            Message message = script.get(0).startMessage;
+            int timeBetweenMessages = message.getMsg().length() * 1000;
+            this.displayTypingEffect();
+
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    System.out.println(getUsername() + ": " + message.getMsg());
+                    if (message.getReplies().isEmpty()){
+                        setOffline();
+                    }
+                }
+            }, timeBetweenMessages);
+        }
+    }
+
+    private void displayTypingEffect() {
+        System.out.println(getUsername() + " is typing...");
+    }
+
+    public void setOffline() {
+        connectionStatus = false;
+        System.out.println(getUsername() + " went offline");
+    }
 }
+
