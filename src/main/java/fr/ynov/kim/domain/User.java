4dad7eee2da99e9bf951eff5150af5a00db0e22c
DantesDels <delver.sebastien@gmail.com>
2025-03-26 15:09:45 +0100

    feat(User): setUsername & setMsgBio done, need setProfilePic with url to choose from.

--- src/main/java/fr/ynov/kim/domain/User.java
+++ src/main/java/fr/ynov/kim/domain/User.java
@@ -1,12 +1,30 @@
 package fr.ynov.kim.domain;
 
+import java.util.Scanner;
+
 public class User extends Person {
 
-    private String preMadeMessages;
+    public User(String username, String profilePicture, String msgBio) {
+        super(username, profilePicture, msgBio);
 
-    public User(String username, String profilPicture, String msgBio) {
-        super(username, profilPicture, msgBio);
+    }
 
+    public void setUsername() {
+        Scanner scanUsername = new Scanner(System.in);
+        System.out.println("Set Username: ");;
+        this.username = scanUsername.nextLine() ;
+    }
 
+    public void setProfilePicture(String profilePicture) {
+        Scanner scanProfilePicture = new Scanner(System.in);
+        // Display all ProfilePictures
+        System.out.println("Choose your ProfilePicture: ");
+        this.profilePicture = scanProfilePicture.nextLine();
+    }
+
+    public void setMsgBio() {
+        Scanner scanMsgBio = new Scanner(System.in);
+        System.out.println("Change your Bio : ");
+        this.msgBio = scanMsgBio.nextLine();
     }
 }
