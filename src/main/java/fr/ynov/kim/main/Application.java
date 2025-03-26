bcec315c884f69635a5bd2844ab2554b0ac9b0db
DantesDels <delver.sebastien@gmail.com>
2025-03-26 15:15:27 +0100

    feat(Application): dev working flow, not launching because of methods and List issues.

--- src/main/java/fr/ynov/kim/main/Application.java
+++ src/main/java/fr/ynov/kim/main/Application.java
@@ -3,14 +3,24 @@ package fr.ynov.kim.main;
 import fr.ynov.kim.domain.FakeUser;
 import fr.ynov.kim.domain.MessageUtils;
 import fr.ynov.kim.domain.Discussion;
+import fr.ynov.kim.domain.User;
+import java.util.List;
 
 public class Application {
 
     public static void main(String[] args) {
-        FakeUser f = MessageUtils.initiliazeUser();
-        Discussion d = f.getScript().get((int)(Math.random() * f.getScript().size()));
-        System.out.println("Chat with : " + f.getUsername());
-        d.start();
 
+        User yourself = new User("Default", "insert_profilePicture", "change_msgBio");
+        yourself.setUsername();
+        yourself.setMsgBio();
+
+        List<FakeUser> fakeUsers =  MessageUtils.initiliazeUser();
+        fakeUsers.forEach(fakeUser -> fakeUser.getScript().get((int)(Math.random() * fakeUser.getScript().size())));
+
+        List<Discussion> discuss = fakeUsers.get((int)(Math.random() * fakeUsers.size())).getScript();
+
+        System.out.println("Chat with : " + fakeUser.getUsername());
+        discuss.start();
+        fakeUser.setTimeToNextConnection();
     }
 }
